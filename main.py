d2a18896824bac59c7a67d80f36d67919cad7c94
Aditya Chowdhary <adityachowdhary04@gmail.com>
2023-04-12 11:25:46 +0530

    Added member join and leave messages

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import os
 load_dotenv()
 disc_token = os.getenv("env_BotTOKEN")
 
-intents = discord.Intents.default()
-intents.message_content = True
+intents = discord.Intents.all()
+
 client = commands.Bot(command_prefix = '!',intents=intents)
 
 @client.event
@@ -16,10 +16,22 @@ async def on_ready():
     print("--------------------")
 
 
-
 @client.command()
 async def hello(ctx):
-    await ctx.send("Hello, this is the bot")
+    user = ctx.author
+    await ctx.send(f"Hello {user}, this is the bot")
+
+
+@client.event
+async def on_member_join(member):
+    channel = client.get_channel(1095340792547119126)
+    await channel.send(f"Hello {member.display_name}")
+    
+
+@client.event
+async def on_member_remove(member):
+    channel = client.get_channel(1095340792547119126)
+    await channel.send(f"Goodbye {member.display_name}")
 
 
 client.run(f'{disc_token}')
